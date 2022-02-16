63c1f99e1983f73f6c176fad7b82fffe75e5ad0e
Matteo Zhang <zhang.matteo00@gmail.com>
2022-02-16 19:59:59 +0100

    Basic setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,7 +42,7 @@ export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
-  },
+  }
 });
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
+  <button @click="count=count*2">Count is: {{count}}</button>
+
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
@@ -7,6 +9,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
@@ -14,5 +17,10 @@ export default defineComponent({
   components: {
     HelloWorld,
   },
-});
+  data() {
+   return {
+    count: 1
+   }
+  }
+  });
 </script>
