1ced62c3dfeb68207362011ecce13576bcb78db7
sheep-tech <56177667+sheep-tech@users.noreply.github.com>
2022-02-28 02:52:57 +0000

    Deploying to gh-pages from  @ 4e6aff51b0d213767bf4694af125a7ac95bcb770 🚀

---
+++ precache-manifest.a2e93b2fe39594ccccd5d5bffcdeda6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42f60e09d99f272eb4fc43734cff4f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46be5672d0a6148f7b30",
+    "url": "/js/about.ab43b202.js"
+  },
+  {
+    "revision": "5fb7bc3e5bba9434ae86",
+    "url": "/js/app.bbed9def.js"
+  },
+  {
+    "revision": "082a33e70eee260db6fa",
+    "url": "/js/chunk-vendors.9fa43be6.js"
+  },
+  {
+    "revision": "4dc9e6f831394ac072fbcc5e327ed20d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
